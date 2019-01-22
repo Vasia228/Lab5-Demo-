10dea5a3078a6627dc4cdbed7b9a10d0f802320e
Vasia228 <43805731+Vasia228@users.noreply.github.com>
2019-01-22 21:38:49 +0300

    Add files via upload

--- data/test.py
+++ data/test.py
@@ -1,13 +1,7 @@
-file=open('students.txt', 'w')
-for i in range(1,31):
-    string="SimpsonBG {}\n".format(i);
-    file.write(string);
-file.close()
-
 file=open('groups.txt', 'w')
 for i in range(3):
     string="group{}".format(i+1);
-    for j in range(i+1,31,3):
+    for j in range(i+1,100,3):
         string=string+" "+"{}".format(j)
     string+="\n"
     file.write(string);
